f58aa8dcf7bcd715fbfd1db06f78d7366f75e696
Kutase <kutase123@gmail.com>
2017-12-08 19:55:18 +0500

    update babel and build

--- out/index.js
+++ out/index.js
@@ -1,10 +1,26 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _regenerator = require('babel-runtime/regenerator');
 
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+var _regenerator2 = _interopRequireDefault(_regenerator);
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+
+var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
+
+var _map = require('babel-runtime/core-js/map');
+
+var _map2 = _interopRequireDefault(_map);
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _createClass2 = require('babel-runtime/helpers/createClass');
+
+var _createClass3 = _interopRequireDefault(_createClass2);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var _require = require('./utils'),
     isFunction = _require.isFunction;
@@ -13,25 +29,25 @@ var ForbiddenError = require('./error');
 
 var BasePermissionManager = function () {
   function BasePermissionManager(user, restrictLiteral) {
-    _classCallCheck(this, BasePermissionManager);
+    (0, _classCallCheck3.default)(this, BasePermissionManager);
 
     this.user = user;
-    this.abilities = new Map([]);
+    this.abilities = new _map2.default([]);
     this.restrictLiteral = restrictLiteral;
   }
 
   // Override in parent class
 
 
-  _createClass(BasePermissionManager, [{
+  (0, _createClass3.default)(BasePermissionManager, [{
     key: 'getModel',
     value: function getModel() {}
   }, {
     key: 'can',
     value: function () {
-      var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(action, target) {
+      var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(action, target) {
         var model, abilityRules, rule;
-        return regeneratorRuntime.wrap(function _callee$(_context) {
+        return _regenerator2.default.wrap(function _callee$(_context) {
           while (1) {
             switch (_context.prev = _context.next) {
               case 0:
@@ -72,9 +88,9 @@ var BasePermissionManager = function () {
   }, {
     key: 'assert',
     value: function () {
-      var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(action, target) {
+      var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(action, target) {
         var model, abilityRules, rule, result;
-        return regeneratorRuntime.wrap(function _callee2$(_context2) {
+        return _regenerator2.default.wrap(function _callee2$(_context2) {
           while (1) {
             switch (_context2.prev = _context2.next) {
               case 0:
@@ -128,7 +144,7 @@ var BasePermissionManager = function () {
     value: function allow(model, action, query, checkMethod) {
       var ability = { query: query, checkMethod: checkMethod };
       if (!this.abilities.has(model)) {
-        this.abilities.set(model, new Map([[action, ability]]));
+        this.abilities.set(model, new _map2.default([[action, ability]]));
       } else {
         var abilityRules = this.abilities.get(model);
 
@@ -142,9 +158,9 @@ var BasePermissionManager = function () {
   }, {
     key: 'accessible',
     value: function () {
-      var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3(model, action) {
+      var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(model, action) {
         var abilityRules, rule;
-        return regeneratorRuntime.wrap(function _callee3$(_context3) {
+        return _regenerator2.default.wrap(function _callee3$(_context3) {
           while (1) {
             switch (_context3.prev = _context3.next) {
               case 0:
@@ -190,7 +206,6 @@ var BasePermissionManager = function () {
       return accessible;
     }()
   }]);
-
   return BasePermissionManager;
 }();
 
